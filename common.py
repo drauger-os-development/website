cf9b02a07d060e3ad4bbdfeacd233b768daa7c25
Thomas Castleman <contact@draugeros.org>
2022-07-06 00:44:03 -0400

    add common lib and new post

---
+++ common.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  common.py
+#
+#  Copyright 2022 Thomas Castleman <contact@draugeros.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+"""Common Functions and variables"""
+import json
+
+# load our settings globally
+with open("settings.json", "r") as file:
+    settings = json.load(file)
+
+
+def contents_in_array(arr1: (list, tuple), arr2: (list, tuple)):
+    """Check if arr2 contains the same contents as arr1"""
+    for each in arr1:
+        if each not in arr2:
+            return False
+    return True
